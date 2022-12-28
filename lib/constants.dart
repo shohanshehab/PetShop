c73ec5184ec317e29ea3a55864176a849a2b36cf
shohan <shehabul35-499@diu.edu.bd>
2022-12-28 19:58:15 +0600

    colour

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-const kPrimaryColor = Color.fromARGB(255, 161, 250, 102);
+const kPrimaryColor = Color.fromARGB(255, 250, 250, 5);
 const kBgColor = Color(0xFF2ebe5);
